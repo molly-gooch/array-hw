77195253916c6108940685d8de060b2e5a963fa0
Mx. Bradford <emma_bradford@milton.com>
2025-01-10 09:20:04 -0500

    Initial commit

---
+++ src/ConeShop.java
@@ -0,0 +1,23 @@
+public class ConeShop {
+
+    public Review[] allReviews;
+    public static void main(String[] args) {
+        //TODO: Make an instance of the main class
+    }
+
+    public ConeShop(){
+        //TODO: Give the array allReviews a size of 10
+
+        //TODO: Fill the array allReviews with reviews with random ratings from 0 to 10 inclusive
+
+        //TODO: print all the ratings in the array allReviews
+        
+        //TODO: Call the method averageRating
+
+    }
+
+    public void averageRating(){
+        //TODO: calculate the average rating from the array allReviews and print it
+
+    }
+}
---
+++ src/Review.java
@@ -0,0 +1,17 @@
+public class Review{
+    
+    public int rating;
+    
+    public Review(int pRating){
+        // TODO: set the instance variable to the parameter
+
+    }
+
+    public void print(){
+        //TODO: use System.out to print the values of the 
+        //      instance varibles so the user can see the review
+
+    }
+
+    
+}
